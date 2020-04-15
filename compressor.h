b79e56fd04fa7269b43659fc7e708cca1821ad05
rivit98 <albert98g@gmail.com>
2020-04-15 18:51:41 +0200

    gui refactored, compressor api

--- compressor.h
+++ compressor.h
@@ -1,15 +1,11 @@
-#pragma once
+#ifndef COMPRESSORAPI_H
+#define COMPRESSORAPI_H
 
-#include "lz4_win32_v1_9_2/include/lz4.h"
-#include "filedata.h"
-#include <QBuffer>
 
-class Compressor
+class CompressorAPI
 {
-private:
-    static constexpr int LZ4_BLOCKSIZE = 1024 * 1024;
 public:
-    Compressor() = default;
-    QByteArray compress(const FileData& f) const;
-    QByteArray uncompress(QByteArray& compressed) const;
+    CompressorAPI();
 };
+
+#endif // COMPRESSORAPI_H
